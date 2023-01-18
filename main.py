5eb490c0f7ea59a41096445709a8dccd5159ac03
piotrksiazek <piotr.ksiazek.mw@gmail.com>
2023-01-18 12:41:45 +0100

    zooming works good! still needs reframing

--- main.py
+++ main.py
@@ -27,50 +27,6 @@ def get_zoom(coordinate, zoom_factor):
         return coordinate + zoom_factor
     return coordinate - zoom_factor
 
-
-def get_new_boundaries(x, y, xmin, xmax, ymin, ymax, zoom):
-    zx = x * (xmax - xmin) / (width - 1) + xmin
-    zy = y * (ymax - ymin) / (height - 1) + ymin
-
-    current_center_x = (xmin + xmax) / 2
-    current_center_y = (ymin + ymax) / 2
-
-    diff_x = abs(current_center_x - zx)
-    diff_y = abs(current_center_y - zy)
-
-    moved_left = False
-    moved_up = False
-
-    if zx < current_center_x:
-        moved_left = True
-
-    if zy > current_center_y:
-        moved_up = True
-
-    if moved_up:
-        ymin += diff_y
-        ymax += diff_y
-    else:
-        ymin -= diff_y
-        ymax -= diff_y
-
-    if moved_left:
-        xmin -= diff_x
-        xmax -= diff_x
-    else:
-        xmin += diff_x
-        xmax += diff_x
-
-    smallest_coordinate = min([abs(xmin), abs(xmax), abs(ymin), abs(ymax)])
-    zoom_factor = smallest_coordinate/2
-
-    xmin = get_zoom(xmin, zoom_factor)
-    xmax = get_zoom(xmax, zoom_factor)
-    ymin = get_zoom(ymin, zoom_factor)
-    ymax = get_zoom(ymax, zoom_factor)
-
-    return xmin, xmax, ymin, ymax
-
 def get_text(xmin, xmax, ymin, ymax, zoom_factor):
     font = pygame.font.SysFont('Helvetica', font_size)
 
@@ -94,26 +50,73 @@ step_increment = 0.05
 running = True
 draw = True
 
+left_mouse_counter = 0
 
+
+class Clicker:
+    def __init__(self):
+        self.xmin = 0
+        self.ymax = 0
+        self.xmax = 0
+        self.ymin = 0
+
+
+
+clicker = Clicker()
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONUP:
             pos = pygame.mouse.get_pos()
-            xmin, xmax, ymin, ymax = get_new_boundaries(pos[0], pos[1], xmin, xmax, ymin, ymax, zoom)
-            draw = True
+            print('first')
+            x = pos[0]
+            y = pos[1]
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                print("left")
-                zoom -= step
-            if event.key == pygame.K_RIGHT:
-                zoom += step
-            if event.key == pygame.K_UP:
-                step += step_increment
-            if event.key == pygame.K_DOWN:
-                step -= step_increment
+            #tutaj jest błąd. One najpierw powinny zostać zpróbkowane i dopiero potem
+            zx = x * (xmax - xmin) / (width - 1) + xmin
+            zy = -(y * (ymax - ymin) / (height - 1) + ymin)
+
+            clicker.xmin = zx
+            clicker.ymax = zy
+
+            while True:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        running = False
+                    if event.type == pygame.MOUSEBUTTONUP:
+                        pos = pygame.mouse.get_pos()
+                        print('second')
+                        x = pos[0]
+                        y = pos[1]
+
+                        # tutaj jest błąd. One najpierw powinny zostać zpróbkowane i dopiero potem
+                        zx = x * (xmax - xmin) / (width - 1) + xmin
+                        zy = -(y * (ymax - ymin) / (height - 1) + ymin)
+
+                        clicker.xmax = zx
+                        clicker.ymin = zy
+
+                        xmin = clicker.xmin
+                        xmax = clicker.xmax
+                        ymin = clicker.ymin
+                        ymax = clicker.ymax
+                        draw = True
+                if draw:
+                    break
+            break
+
+
+        # if event.type == pygame.KEYDOWN:
+        #     if event.key == pygame.K_LEFT:
+        #         print("left")
+        #         zoom -= step
+        #     if event.key == pygame.K_RIGHT:
+        #         zoom += step
+        #     if event.key == pygame.K_UP:
+        #         step += step_increment
+        #     if event.key == pygame.K_DOWN:
+        #         step -= step_increment
 
     if draw:
         # Generate the Mandelbrot set
