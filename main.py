26a32bbf8fce808941a002efc4c9300922ba3149
piotrksiazek <piotr.ksiazek.mw@gmail.com>
2023-02-01 15:51:51 +0100

    readme

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ width, height = 800, 800
 xmin, xmax = -2.0, 2.0
 ymin, ymax = -2.0, 2.0
 max_iter = 255
+iteration_change_step = 20
 
 # Initialize Pygame
 pygame.init()
@@ -24,18 +25,33 @@ font_size = 15
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Mandelbrot Set')
 
+
 def get_text(xmin, xmax, ymin, ymax):
     font = pygame.font.SysFont('Helvetica', font_size)
 
     first_text = font.render(f'xmin: {xmin} xmax: {xmax}', True, (255, 255, 255))
     second_text = font.render(f'ymin: {ymin} ymax: {ymax}', True, (255, 255, 255))
+    third_text = font.render(f'max iterations: {max_iter}', True, (255, 255, 255))
+    fourth_text = font.render(f'iteration change step: {iteration_change_step}', True, (255, 255, 255))
 
     all_text_x = 10
     first_text_y = 10
     second_text_y = first_text_y + 1.5 * font_size
+    third_text_y = second_text_y + 1.5 * font_size
+    fourth_text_y = third_text_y + 1.5 * font_size
+
+    width = max(first_text.get_size()[0], second_text.get_size()[0], third_text.get_size()[0], fourth_text.get_size()[0])
+    height = (first_text.get_size()[1] + second_text.get_size()[1] + third_text.get_size()[1] + fourth_text.get_size()[1]) * 1.6
+
+    width = 1.2 * width
+
+    background = pygame.Rect(0, 0, width, height)
+    pygame.draw.rect(screen, (0, 0, 0), background)
 
     screen.blit(first_text, (all_text_x, first_text_y))
     screen.blit(second_text, (all_text_x, second_text_y))
+    screen.blit(third_text, (all_text_x, third_text_y))
+    screen.blit(fourth_text, (all_text_x, fourth_text_y))
 
 running = True
 draw = True
@@ -43,7 +59,7 @@ draw = True
 left_mouse_counter = 0
 
 
-class Clicker:
+class Boundaries:
     def __init__(self):
         self.xmin = 0
         self.ymax = 0
@@ -51,51 +67,58 @@ class Clicker:
         self.ymin = 0
 
 
-clicker = Clicker()
+boundaries = Boundaries()
+click_count = 0
 while running:
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.MOUSEBUTTONUP:
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                max_iter += iteration_change_step
+            elif event.key == pygame.K_LEFT:
+                if (max_iter - iteration_change_step) > 0:
+                    max_iter -= iteration_change_step
+            elif event.key == pygame.K_UP:
+                iteration_change_step += 20
+            elif event.key == pygame.K_DOWN:
+                if (iteration_change_step - 20) > 0:
+                    iteration_change_step -= 20
+
+        elif event.type == pygame.MOUSEBUTTONUP:
             pos = pygame.mouse.get_pos()
 
             x = pos[0]
             y = pos[1]
 
-            zx = x * (xmax - xmin) / (width - 1) + xmin
-            zy = -(y * (ymax - ymin) / (height - 1) + ymin)
+            if click_count == 1:
+                zx = x * (xmax - xmin) / (width - 1) + xmin
+                zy = -(y * (ymax - ymin) / (height - 1) + ymin)
 
-            clicker.xmin = zx
-            clicker.ymax = zy
+                boundaries.xmax = zx
+                boundaries.ymin = zy
 
-            while True:
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        running = False
-                    if event.type == pygame.MOUSEBUTTONUP:
-                        pos = pygame.mouse.get_pos()
+                xmin = boundaries.xmin
+                xmax = boundaries.xmax
+                ymin = boundaries.ymin
+                ymax = boundaries.ymax
+                draw = True
 
-                        x = pos[0]
-                        y = pos[1]
+            if click_count == 0:
+                zx = x * (xmax - xmin) / (width - 1) + xmin
+                zy = -(y * (ymax - ymin) / (height - 1) + ymin)
 
-                        zx = x * (xmax - xmin) / (width - 1) + xmin
-                        zy = -(y * (ymax - ymin) / (height - 1) + ymin)
+                boundaries.xmin = zx
+                boundaries.ymax = zy
 
-                        clicker.xmax = zx
-                        clicker.ymin = zy
-
-                        xmin = clicker.xmin
-                        xmax = clicker.xmax
-                        ymin = clicker.ymin
-                        ymax = clicker.ymax
-                        draw = True
-                if draw:
-                    break
-            break
+                click_count += 1
 
 
     if draw:
         # Generate the Mandelbrot set
+        click_count = 0
+
         for x in range(width):
             for y in range(height):
                 c = complex(xmin + (x / width) * (xmax - xmin),
@@ -113,7 +136,7 @@ while running:
                 hsl = i / max_iter
                 r, g, b = colorsys.hsv_to_rgb(hsl, hsl, hsl)
 
-                screen.set_at((x, y), (r * max_iter, g * max_iter, b * max_iter))
+                screen.set_at((x, y), (int(r * 255), int(g * 255), int(b * 255)))
 
     draw = False
 
