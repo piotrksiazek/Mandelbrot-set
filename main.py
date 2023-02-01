4126a588bb53bf1f7eaf6c5d20d9188d4cdfc33d
piotrksiazek <piotr.ksiazek.mw@gmail.com>
2023-02-01 15:04:50 +0100

    color palette

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
+#uwagi
+#użyć rect do punktów !
+#wybór liczby iteracji #
+#wyświetlanie kolorów #
+#readme domena i instrukcja
+
 import pygame
+import colorsys
 
 width, height = 800, 800
 
-xmin, xmax = -1.0, 1.0
-ymin, ymax = -1.0, 1.0
-max_iter = 400
+xmin, xmax = -2.0, 2.0
+ymin, ymax = -2.0, 2.0
+max_iter = 255
 
 # Initialize Pygame
 pygame.init()
@@ -14,28 +21,21 @@ pygame.font.init()
 font_size = 15
 
 # window
-screen = pygame.display.set_mode((width,height))
+screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Mandelbrot Set')
 
-def get_text(xmin, xmax, ymin, ymax, zoom_factor):
+def get_text(xmin, xmax, ymin, ymax):
     font = pygame.font.SysFont('Helvetica', font_size)
 
     first_text = font.render(f'xmin: {xmin} xmax: {xmax}', True, (255, 255, 255))
     second_text = font.render(f'ymin: {ymin} ymax: {ymax}', True, (255, 255, 255))
-    third_text = font.render(f'zoom factor: {str(zoom_factor)[:5]}', True, (255, 255, 255))
 
     all_text_x = 10
     first_text_y = 10
     second_text_y = first_text_y + 1.5 * font_size
-    third_text_y = second_text_y + 1.5 * font_size
 
     screen.blit(first_text, (all_text_x, first_text_y))
     screen.blit(second_text, (all_text_x, second_text_y))
-    screen.blit(third_text, (all_text_x, third_text_y))
-
-zoom = 0.1
-step = 0.05
-step_increment = 0.05
 
 running = True
 draw = True
@@ -74,11 +74,10 @@ while running:
                         running = False
                     if event.type == pygame.MOUSEBUTTONUP:
                         pos = pygame.mouse.get_pos()
-                        print('second')
+
                         x = pos[0]
                         y = pos[1]
 
-                        # tutaj jest błąd. One najpierw powinny zostać zpróbkowane i dopiero potem
                         zx = x * (xmax - xmin) / (width - 1) + xmin
                         zy = -(y * (ymax - ymin) / (height - 1) + ymin)
 
@@ -103,18 +102,20 @@ while running:
                             ymin + (y / height) * (ymax - ymin))
 
                 z = 0
-                for i in range(max_iter):
+                i = 0
+                for _ in range(max_iter):
+                    i += 1
+
                     if abs(z) > 2.0:
                         break
                     z = z * z + c
-                # Color the pixel red at full iteration
-                if i > max_iter:
-                    i = 0
-                r, g, b = i % 128, 0, 0
-                screen.set_at((x, y), (r, g, b))
-        max_iter += 10
+
+                hsl = i / max_iter
+                r, g, b = colorsys.hsv_to_rgb(hsl, hsl, hsl)
+
+                screen.set_at((x, y), (r * max_iter, g * max_iter, b * max_iter))
 
     draw = False
 
-    get_text(xmin, xmax, ymin, ymax, zoom)
+    get_text(xmin, xmax, ymin, ymax)
     pygame.display.flip()
